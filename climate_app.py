7078766c44b32478d8ce01bd7c48a5c982400e38
Noe Torres <ntorr012@ucr.edu>
2020-08-21 00:26:09 -0700

    finally done

--- climate_app.py
+++ climate_app.py
@@ -7,6 +7,8 @@ from sqlalchemy import create_engine, func
 
 from flask import Flask, jsonify
 
+import datetime as dt
+
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
@@ -21,34 +23,121 @@ stations = Base.classes.station
 # Create our session (link) from Python to the DB
 session = Session(engine)
 
-app = Flask(__Climate__)
+app = Flask(__name__)
 
+#home route
 @app.route("/")
 def home():
-    print("Available routes:")
-    print("/api/v1.0/precipitation")
-    print("/api/v1.0/stations")
-    print("/api/v1.0/tobs")
-    print("/api/v1.0/<start>` and `/api/v1.0/<start>/<end>")
+    return(
+        f'Available routes:<br/>'
+        f'/api/v1.0/precipitation<br/>'
+        f'/api/v1.0/stations<br/>'
+        f'/api/v1.0/tobs<br/>'
+        f'/api/v1.0/start and /api/v1.0/start/end'
+    )
 
+#route for precip info
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-    results = session.query(measurements.date, measurements.prcp, measurements.tobs)
-    print("")
+    
+    #query the date and precip data
+    results = session.query(measurements.date, measurements.prcp).all()
+    session.close()
 
+     # lists for the dictionary
+    date_list = []
+    prcp_list = []
+    
+    #loop to fill in the lists
+    for date, prcp in results:
+        date_list.append(date)
+        prcp_list.append(prcp)
+    
+    #create the dictionary
+    prcp_dict = dict(zip(date_list, prcp_list))
+
+    #return the jsonified list
+    return jsonify(prcp_dict)
+
+#route for station info
 @app.route("/api/v1.0/stations")
-def precipitation():
-    print("")
+def stations_json():
+    
+    #query the station results
+    results = session.query(stations.station).all()
+    session.close()
+    
+    #list for station info
+    station_list = []
+    
+    #loop to collect the station info
+    for station in results:
+        station_list.append(station)
+    
+    #return the jsonified list
+    return jsonify(station_list)
 
+#route for temp info
 @app.route("/api/v1.0/tobs")
-def precipitation():
-    print("")
+def temperature():
     
+    # Calculate the date 1 year ago from the last data point in the database
+    query_date = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    
+    #query results
+    results = session.query(measurements.date, measurements.tobs).\
+        filter(measurements.date >= query_date).\
+        filter(measurements.station=='USC00519281')
+    session.close()
+    
+    #list for the temp info
+    tobs_list = []
+    
+    #loop to collect temp info
+    for tobs in results:
+        tobs_list.append(tobs)
+    
+    #return the jsonified list
+    return jsonify(tobs_list)
+    
+#route for temp info based on start date
 @app.route("/api/v1.0/<start>")
-def precipitation():
-    print("")
+def temp_start(start):
     
-@app.route("/api/v1.0/<start>/<end>")
-def precipitation():
-    print("")
+    #query results based on start date
+    results = session.query(measurements.date, measurements.tobs).\
+        filter(measurements.date >= start)
+    session.close()
+    
+    #list for temp data
+    tobs_list = []
+    
+    #loop to collect data for temp data
+    for tobs in results:
+        tobs_list.append(tobs)
 
+    #return the jsonified list
+    return jsonify(tobs_list)
+    
+#route for temp data based on start and end dates
+@app.route("/api/v1.0/<start>/<end>")
+def temp_segment(start, end):
+    
+    #query the temp data
+    results = session.query(measurements.date, measurements.tobs).\
+        filter(measurements.date >= start).\
+        filter(measurements.date <= end)
+    session.close()
+    
+    #list for the temo data
+    tobs_list = []
+    
+    #loop to save the temp results
+    for tobs in results:
+        tobs_list.append(tobs)
+    
+    #return the jsonified list
+    return jsonify(tobs_list)
+
+if __name__ == '__main__':
+    app.run(debug=True)
